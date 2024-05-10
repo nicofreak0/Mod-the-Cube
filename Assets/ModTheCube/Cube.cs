34343fba7737bd0058ab0fb14a2e647c4b49805a
Lydia <146027704+nicofreak0@users.noreply.github.com>
2024-05-10 15:15:57 -0500

    i need to edit script

--- Assets/ModTheCube/Cube.cs
+++ Assets/ModTheCube/Cube.cs
@@ -5,17 +5,21 @@ using UnityEngine;
 public class Cube : MonoBehaviour
 {
     public MeshRenderer Renderer;
+   
     
     void Start()
     {
-        transform.position = new Vector3(3, 4, 1);
+       
+        transform.position = new Vector3(2, 7, 1);
         transform.localScale = Vector3.one * 1.3f;
         
+        Material material = Renderer.material;
         
+        material.color = new Color(0.5f, 1.0f, 0.3f, 0.4f);
     }
     
     void Update()
     {
-        transform.Rotate(10.0f * Time.deltaTime, 0.0f, 0.0f);
+        transform.Rotate(20.0f * Time.deltaTime, 0.0f, 0.0f);
     }
 }
