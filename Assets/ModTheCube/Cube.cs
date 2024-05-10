47bb73f764ed71a0dc306488809d4ce6d0219798
Lydia <146027704+nicofreak0@users.noreply.github.com>
2024-05-10 14:56:14 -0500

    Initial commit

---
+++ Assets/ModTheCube/Cube.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public MeshRenderer Renderer;
+    
+    void Start()
+    {
+        transform.position = new Vector3(3, 4, 1);
+        transform.localScale = Vector3.one * 1.3f;
+        
+        
+    }
+    
+    void Update()
+    {
+        transform.Rotate(10.0f * Time.deltaTime, 0.0f, 0.0f);
+    }
+}
