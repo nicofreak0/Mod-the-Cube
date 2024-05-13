521af78b114bd784df9b63521c33a48f60788984
Lydia <146027704+nicofreak0@users.noreply.github.com>
2024-05-13 13:10:06 -0500

    script

--- Assets/ModTheCube/Cube.cs
+++ Assets/ModTheCube/Cube.cs
@@ -5,21 +5,48 @@ using UnityEngine;
 public class Cube : MonoBehaviour
 {
     public MeshRenderer Renderer;
-   
-    
+    // public Vector3 Pos = new Vector3 (0, 0, 0);
+    public Vector3 scaleChange = new Vector3(5, 5, 5);
+    public float scaleTimes = 1.5f;
+    private Color color;
+    private Vector3 rotate;
+    private bool col = true;
+
     void Start()
     {
-       
-        transform.position = new Vector3(2, 7, 1);
-        transform.localScale = Vector3.one * 1.3f;
-        
-        Material material = Renderer.material;
-        
-        material.color = new Color(0.5f, 1.0f, 0.3f, 0.4f);
+        transform.localScale = scaleChange * scaleTimes;
+
+        // Material material = Renderer.material; 
+
+        // material.color = color;
     }
-    
+
     void Update()
     {
-        transform.Rotate(20.0f * Time.deltaTime, 0.0f, 0.0f);
+
+        if (col == true)
+        {
+            float randomColorR = Random.Range(0.0f, 1.0f);
+            float randomColorG = Random.Range(0.0f, 1.0f);
+            float randomColorB = Random.Range(0.0f, 1.0f);
+            float randomColorA = Random.Range(0.0f, 1.0f);
+            Material material = Renderer.material;
+            material.color = new Color(randomColorR, randomColorG, randomColorB, randomColorA);
+            col = false;
+            // Debug.Log(randomColorR);
+            Invoke("ColorChange", 1.0f);
+        }
+
+        float rotationX = Random.Range(2.0f, 10.0f);
+        float rotationY = Random.Range(2.0f, 10.0f);
+        float rotationZ = Random.Range(2.0f, 10.0f);
+        transform.Rotate(rotationX * Time.deltaTime, rotationY * Time.deltaTime, rotationZ * Time.deltaTime);
+    }
+
+   
+
+    void ColorChange()
+    {
+        col = true;
     }
 }
